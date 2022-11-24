8113252de8cbe6298d97e5568729b050967d20ba
Seonghun <77530278+kawai10@users.noreply.github.com>
2022-11-24 15:16:24 +0900

    ApolloProvider

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,5 +1,32 @@
+import { gql, useApolloClient } from '@apollo/client';
+import { useEffect, useState } from 'react';
+
 const Movies = () => {
-	return <div>This is movies page</div>;
+	const [movies, setMovies] = useState([]);
+	const client = useApolloClient();
+
+	useEffect(() => {
+		client
+			.query({
+				query: gql`
+					query {
+						allMovies {
+							title
+							id
+						}
+					}
+				`,
+			})
+			.then(response => setMovies(response.data.allMovies));
+	}, [client]);
+
+	return (
+		<div>
+			{movies.map(movie => (
+				<li key={movie.id}>{movie.title}</li>
+			))}
+		</div>
+	);
 };
 
 export default Movies;
